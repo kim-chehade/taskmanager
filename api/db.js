90ce6f040fdf147babcfa2c09ad687580ae73a04
Karim Chehade <kim.chehade@gmail.com>
2023-10-28 11:17:22 +0300

    edited

--- api/db.js
+++ api/db.js
@@ -1,8 +1,8 @@
 import mysql from "mysql";
 
 export const db = mysql.createConnection({
-    host:"sql11.freemysqlhosting.net",
-    user:"sql11657359",
-    password:"FsUkz93FQM",
-    database:"sql11657359",
+    host:"localhost",
+    user:"root",
+    password:"root123",
+    database:"tmdb",
 })
